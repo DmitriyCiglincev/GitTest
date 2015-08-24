85e695927830a2b7cde4131dd666ab0926c5c308
phoen111 <fenixx-111@mail.ru>
2015-08-24 23:03:24 +0300

    New string

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello GitHub!!!! I'm here!");
         System.out.println("This is phoen");
+        System.out.println("New string");
     }
 }
